fac555f6e78933fb72365cbeb01244dc6a4a87b4
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2021-03-06 15:58:48 +0200

    solvingproblemtwo

--- app.js
+++ app.js
@@ -32,12 +32,17 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b) {
+    let service = a * b;
+  let myArray = [
+    service,
+    "the multiply of " + a + " and " + b + " is " + service];
+  return myArray;
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
